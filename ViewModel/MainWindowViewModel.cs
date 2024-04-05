efe6681bcc0fe614f137853e19219e2e9d17f8d4
=DedePomer <=t343st@gmail.com>
2024-04-05 16:51:07 +0300

    Исправил прикол со страницами

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WPF_SQL.Infrastructure.Navigation;
+using WPF_SQL.View.Pages.Tests;
 using WPF_SQL.ViewModel.Base;
 
 namespace WPF_SQL.ViewModel
