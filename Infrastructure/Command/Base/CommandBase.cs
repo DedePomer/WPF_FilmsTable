19e1e2d79ec12f5c8739af4e507ab9038bc4700f
=DedePomer <=t343st@gmail.com>
2024-04-05 18:56:57 +0300

    Добавил ьазовую команду

---
+++ Infrastructure/Command/Base/CommandBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_SQL.Infrastructure.Command.Base
+{
+    internal abstract class CommandBase :ICommand
+    {
+        public event EventHandler? CanExecuteChanged // Выполнятется если поменялось на ложь метод CanExecute
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object? parameter); // Ложь - команду выполнить нельзя; Истина - можно (может включать и выключать работу определённых элементов
+
+        public abstract void Execute(object? parameter); //Логика комманды
+    }
+}
