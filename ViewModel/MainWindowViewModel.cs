734f87f371e50e58f864d0a11a199e7367152a61
=DedePomer <=t343st@gmail.com>
2024-04-05 16:06:02 +0300

    Добавил ViewModelBase

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_SQL.ViewModel.Base;
+
+namespace WPF_SQL.ViewModel
+{
+    internal class MainWindowViewModel: ViewModelBase
+    {
+
+    }
+}
