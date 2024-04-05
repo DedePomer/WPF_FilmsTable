8779a4aacd12058d56e7844a9d29c582f75656db
=DedePomer <=t343st@gmail.com>
2024-04-05 16:41:47 +0300

    Создал класс NavigationStore, который хранит в себе тикущую View

---
+++ Infrastructure/Navigation/NavigationStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_SQL.ViewModel.Base;
+
+namespace WPF_SQL.Infrastructure.Navigation
+{
+    internal class NavigationStore
+    {
+        private ViewModelBase? _currentViewModel;
+
+        public ViewModelBase? CurrentViewModel {             
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+            }
+        }
+    }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_SQL.Infrastructure.Navigation;
 using WPF_SQL.ViewModel.Base;
 
 namespace WPF_SQL.ViewModel
 {
     internal class MainWindowViewModel: ViewModelBase
     {
-
+        // Навигация
+        NavigationStore navigationStore = new NavigationStore();
+        public ViewModelBase? CurrentViewModel => navigationStore.CurrentViewModel;
     }
 }
